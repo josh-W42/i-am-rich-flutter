e641bb543d40596504d3dd51ffc1fba46ba862d7
josh-W42 <wilson.jd022@gmail.com>
2024-11-29 12:34:09 -0800

    Added the diamond.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 
 /// Reformating can occur on save but ideally you should
-/// f
+/// familiarize yourself with the expected format.
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold()));
+  runApp(MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          appBar: AppBar(
+            title: const Text("I am Rich"),
+            backgroundColor: Colors.blueAccent,
+          ),
+          body: const Center(
+            child: Image(image: AssetImage('images/diamond.png')),
+          ))));
 }
+
+// // A way to get an external image by providing a URL
+// image: NetworkImage(
+//     'https://gratisography.com/wp-content/uploads/2024/10/gratisography-cool-cat-800x525.jpg')),
