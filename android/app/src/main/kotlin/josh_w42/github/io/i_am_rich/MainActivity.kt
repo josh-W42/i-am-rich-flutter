bb8ab3344461241b021ac3f2c04da6257b2db210
josh-W42 <wilson.jd022@gmail.com>
2024-11-29 09:00:14 -0800

    init git repo

---
+++ android/app/src/main/kotlin/josh_w42/github/io/i_am_rich/MainActivity.kt
@@ -0,0 +1,5 @@
+package josh_w42.github.io.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+/// Reformating can occur on save but ideally you should
+/// f
+
+void main() {
+  runApp(const MaterialApp(home: Scaffold()));
+}
